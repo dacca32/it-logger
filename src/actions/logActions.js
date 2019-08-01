567c10e38569dec94c957866cf98427808613f38
Scot Darcy <scot.darcy@nccgroup.trust>
2019-08-01 14:29:19 +0100

    implemented search functionality

--- src/actions/logActions.js
+++ src/actions/logActions.js
@@ -1,4 +1,4 @@
-import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG } from './types';
+import { GET_LOGS, SET_LOADING, LOGS_ERROR, ADD_LOG, DELETE_LOG, SET_CURRENT, CLEAR_CURRENT, UPDATE_LOG, SEARCH_LOGS } from './types';
 
 
 // export const getLogs = () => {
@@ -111,6 +111,29 @@ export const updateLog = (log) => async dispatch => {
     const data = await res.json();
     dispatch({
       type: UPDATE_LOG,
+      payload: data
+    })
+  } catch (error) {
+
+    dispatch({
+      type: LOGS_ERROR,
+      payload: error.response.data
+    })
+
+  }
+}
+
+// Search Logs
+
+export const searchLogs = (text) => async dispatch => {
+  // redux thunk allows us to return a function
+  try {
+    setLoading();
+    const res = await fetch(`/logs?q=${text}`)
+    const data = await res.json();
+
+    dispatch({
+      type: SEARCH_LOGS,
       payload: data
     })
   } catch (error) {
